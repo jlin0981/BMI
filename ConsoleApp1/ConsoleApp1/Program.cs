d35d5132242b94d18f943c793f4a4482e97ac772
DESKTOP-MUHELDF\james <a106082035@mail.shu.edu.tw>
2018-04-21 12:56:24 +0800

    IF更改

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -26,39 +26,39 @@ namespace ConsoleApp1
             BMI = w / ( h * h ) ;
 
             // 判斷 BMI 的資訊
-            if ( BMI < 16.5 ) 
-            { Console.WriteLine( " 免役體位 & 體重過輕 " ) ; }
+            if (BMI < 16.5)
+                Console.WriteLine(" 免役體位 & 體重過輕 ");
 
-            else if (BMI >= 16.5  && BMI < 17)
-            { Console.WriteLine( " 替代役體位 & 體重過輕 " ) ; }
+            else if (BMI >= 16.5 && BMI < 17)
+                Console.WriteLine(" 替代役體位 & 體重過輕 ");
 
             else if (BMI >= 17 && BMI < 18.5)
-            { Console.WriteLine( " 常備役體位 & 體重過輕 " ) ; }
+                Console.WriteLine(" 常備役體位 & 體重過輕 ");
 
             else if (BMI >= 18.5 && BMI < 24)
-            { Console.WriteLine( " 常備役體位 & 體重正常 " ) ; }
+                Console.WriteLine(" 常備役體位 & 體重正常 ");
 
             else if (BMI >= 24 && BMI < 27)
-            { Console.WriteLine( " 常備役體位 & 體重過重 " ); }
+                Console.WriteLine(" 常備役體位 & 體重過重 ");
 
             else if (BMI >= 27 && BMI < 30)
-            { Console.WriteLine( " 常備役體位 & 輕度肥胖 " ); }
+                Console.WriteLine(" 常備役體位 & 輕度肥胖 ");
 
             else if (BMI >= 30 && BMI < 31)
-            { Console.WriteLine( " 常備役體位 & 中度肥胖 " ); }
+                Console.WriteLine(" 常備役體位 & 中度肥胖 ");
 
             else if (BMI >= 31 && BMI < 31.5)
-            { Console.WriteLine( " 替代役體位 & 中度肥胖 " ); }
+                Console.WriteLine(" 替代役體位 & 中度肥胖 ");
 
             else if (BMI >= 31.5 && BMI < 35)
-            { Console.WriteLine( " 免役體位 & 中度肥胖 " ); }
+                Console.WriteLine(" 免役體位 & 中度肥胖 ");
 
-            else if (BMI >= 35 )
-            { Console.WriteLine( " 免役體位 & 重度肥胖 " ); }
+            else if (BMI >= 35)
+                Console.WriteLine(" 免役體位 & 重度肥胖 "); 
 
-            // 暫停 & 任意鍵結束
-            Console.ReadLine();
+                // 暫停 & 任意鍵結束
+                Console.ReadLine();
 
-        }
+            }
     }
 }
